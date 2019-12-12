37dbfe19a2334c42b7bf5d5c55795988476ac8fe
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-12 21:13:23 +0530

    Updates

---
+++ precache-manifest.92651d44330f31158a9e8b85bba5c69d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767c1edbfed1d2342e457b191875f1e0",
+    "url": "/react-project-rolodex/index.html"
+  },
+  {
+    "revision": "9cac76b9adfc33f9cdea",
+    "url": "/react-project-rolodex/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c5cea51c532f3706594b",
+    "url": "/react-project-rolodex/static/css/main.79348bbc.chunk.css"
+  },
+  {
+    "revision": "9cac76b9adfc33f9cdea",
+    "url": "/react-project-rolodex/static/js/2.41950185.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-project-rolodex/static/js/2.41950185.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5cea51c532f3706594b",
+    "url": "/react-project-rolodex/static/js/main.4bda6412.chunk.js"
+  },
+  {
+    "revision": "a99bf4601966a159e9e8",
+    "url": "/react-project-rolodex/static/js/runtime-main.72d7ed51.js"
+  }
+]);
